eb22c172c2422ae214f873935c705f61905a7710
Lena <summyalena@gmail.com>
2022-09-29 20:02:03 +0100

    made updates to the profile file

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import JoinedCard from './joinedCard';
 
 function MyProfile() {
+  const { missions } = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.joined === true);
+  const toDisplay = joinedMissions.map((mission) => (
+    <JoinedCard key={mission.id} name={mission.name} />));
   return (
-    <div>MyProfile</div>
+    <div>
+      <div>
+        {
+          toDisplay
+        }
+      </div>
+    </div>
   );
 }
 
