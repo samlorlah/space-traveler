495592ed7b46978412a192464f36887c8401f0ae
Lena <summyalena@gmail.com>
2022-09-29 19:59:11 +0100

    made updates to joinedCard.js

---
+++ src/components/joinedCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+function JoinedCard({ name }) {
+  return (
+    <div>{name}</div>
+  );
+}
+
+export default JoinedCard;
