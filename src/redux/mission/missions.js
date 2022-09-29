77bb535f4ea558b0adeb2f3cbfb027ad384198e5
Lena <summyalena@gmail.com>
2022-09-29 20:03:51 +0100

    made updates to the redux file for join and leave functionality

--- src/redux/mission/missions.js
+++ src/redux/mission/missions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -5,7 +6,7 @@ const API = 'https://api.spacexdata.com/v3/missions';
 
 export const fetchMission = createAsyncThunk('fetch-data', async () => {
   const response = await axios.get(API);
-  const data = await response.data;
+  const { data } = response;
   const mission = data.map((object) => ({
     id: object.mission_id,
     name: object.mission_name,
@@ -17,31 +18,42 @@ export const fetchMission = createAsyncThunk('fetch-data', async () => {
 
 const initialState = {
   missions: [],
-  loading: false,
+  loading: 'idle',
 };
 const missions = createSlice({
   name: 'missions',
   initialState,
   reducers: {
     joinMission(state, action) {
+      console.log(action);
       // eslint-disable-next-line max-len
-      console.log(state.missions);
-      state.missions.find((mission) => (mission.id === action.payload.id
-        ? { ...mission, joined: true }
-        : mission));
-      console.log(state.missions);
+      state.missions = state.missions.map((obj) => {
+        if (obj.id === action.payload) {
+          obj.joined = true;
+          return obj;
+        }
+        return obj;
+      });
+      return state;
+    },
+    leaveMission(state, action) {
+      console.log(action);
+      // eslint-disable-next-line max-len
+      state.missions = state.missions.map((obj) => {
+        if (obj.id === action.payload) {
+          obj.joined = false;
+          return obj;
+        }
+        return obj;
+      });
+      return state;
     },
   },
   extraReducers: {
-    [fetchMission.pending]: (state) => {
-    // eslint-disable-next-line no-param-reassign
-      state.loading = true;
-    },
     [fetchMission.fulfilled]: (state, action) => {
-      // eslint-disable-next-line no-param-reassign
-      state.loading = false;
-      // eslint-disable-next-line no-param-reassign
+      console.log(action);
       state.missions = action.payload;
+      state.loading = 'success';
     },
   },
 });
