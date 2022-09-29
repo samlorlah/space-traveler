ed0e5b88dba31bce03e24bee8b87530ff7184027
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-09-29 13:21:07 +0100

    Use State to render in Rocket components and trigger events.

--- src/components/RocketsItemList.js
+++ src/components/RocketsItemList.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RocketsItemList = () => (
+const RocketsItemList = ({
+  id,
+  name,
+  description,
+  image,
+  reserved,
+  handleReservation,
+  handleReservationCancelation,
+}) => (
   <li>
     <div className="images">
-      <img src="https://via.placeholder.com/200" alt="" />
+      <img height={150} width={200} src={image} alt={`${name}`} />
     </div>
     <div className="description">
-      <h4>Falcon 1</h4>
+      <h4>{name}</h4>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-        specimen book. It has survived not only five centuries, but also the leap into
-        electronic typesetting, remaining essentially unchanged. It was popularised in
-        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-        and more recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.
+        {reserved && <span>Reserved</span>}
+        {' '}
+        {description}
       </p>
-      <button className="reserveRocketBtn" type="button">Reserve Comment</button>
+      {!reserved && (
+        <button className="reserveRocketBtn" onClick={() => handleReservation(id)} type="button">Reserve Rocket</button>
+      )}
+      {reserved && (
+        <button className="cancelRocketBtn" onClick={() => handleReservationCancelation(id)} type="button">Cancel Reservation</button>
+      )}
     </div>
   </li>
 );
+
+RocketsItemList.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
+  handleReservation: PropTypes.func.isRequired,
+  handleReservationCancelation: PropTypes.func.isRequired,
+};
 
 export default RocketsItemList;
